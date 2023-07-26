814ae9a0d6172306d7c114791cee6170fe7a4176
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-26 12:45:51 -0300

    Create ClientDTO

--- HomeBanking/Models/ClientDTO.cs
+++ HomeBanking/Models/ClientDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace HomeBanking.Models
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace HomeBanking.Models
 {
     public class ClientDTO
     {
+        [JsonIgnore]
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<AccountDTO> Accounts { get; set; }
     }
 }
