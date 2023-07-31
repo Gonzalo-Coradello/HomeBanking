2f0c17d77b4ce8a2d44c4c2e9e81554acd84668d
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-31 12:15:30 -0300

    Modify ClientRepository

--- HomeBanking/Repositories/ClientRepository.cs
+++ HomeBanking/Repositories/ClientRepository.cs
@@ -10,11 +10,19 @@ namespace HomeBanking.Repositories
         public ClientRepository(HomeBankingContext repositoryContext) : base(repositoryContext) { }
         public Client FindById(long id)
         {
-            return FindByCondition(client => client.Id == id).Include(client => client.Accounts).FirstOrDefault();
+            return FindByCondition(client => client.Id == id)
+                .Include(client => client.Accounts)
+                .Include(client => client.ClientLoans)
+                    .ThenInclude(cl => cl.Loan)
+                .FirstOrDefault();
         }
         public IEnumerable<Client> GetAllClients()
         {
-            return FindAll().Include(client => client.Accounts).ToList();
+            return FindAll()
+                .Include(client => client.Accounts)
+                .Include(client => client.ClientLoans)
+                    .ThenInclude(cl => cl.Loan)
+                .ToList();
         }
         public void Save(Client client)
         {
